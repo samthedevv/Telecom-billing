c1e16eef991d966460ac00f24f227f5e141fa31e
francis19-py <francisgithambo80@gmail.com>
2025-03-24 18:55:32 +0300

    comments

--- Structure.c
+++ Structure.c
@@ -2,15 +2,17 @@
 #include <string.h>
 
 //struct holding customer information
+
 struct customer {
     char name;
     char phoneNumber;
     float usage;
     float totalBill;
 };
-//array to store customer data
+//array to store customer data.
 struct customer customers[100] ;
 
-//global variable to keep track of number of customers
+//global variable for customers
+
 int customerCount = 0;
  
