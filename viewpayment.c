fbd179341a965988a4e35defb6ad5298c063b4ea
Dennis <dennisomondi188@gmail.com>
2025-04-08 13:07:24 +0300

    function to viewpayment

--- viewpayment.c
+++ viewpayment.c
@@ -1,5 +1,11 @@
-#include <stdio.h>
-int main(){
-    printf("hello");
-    return 0;
+//Function to viewpayment for a customer
+void viewpayment (char phoneNumber[])
+{
+    for (int i =0; i < customerCount; i++){
+        if (strcmp(customers[i].phoneNumber, phoneNumber)==0){
+            printf("\nTotal Bill for %s: $%.2f\n");
+            return;
+        }
+    }
+     printf("\nRecord not found!\n");
 }
