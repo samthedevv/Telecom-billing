5003c4180bac6cffdedc5cce51fdcbea585bed68
francis19-py <francisgithambo80@gmail.com>
2025-04-23 20:25:21 +0300

    Update Deleterecords.c

--- Deleterecords.c
+++ Deleterecords.c
@@ -1,4 +1,4 @@
-void deleteRecord(char phoneNumber[]) { 
+void deleteRecord(const char *phoneNumber) { 
 	for (int i = 0; i < customerCount; i++) { 
 		if (strcmp(customers[i].phoneNumber, phoneNumber) == 0) { 
 			for (int j = i; j < customerCount - 1; j++) { 
