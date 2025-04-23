476d5c6405cd846a3ca4004ba655c51047daf69d
francis19-py <francisgithambo80@gmail.com>
2025-04-23 20:18:18 +0300

    error handling

--- modifyrecords.c
+++ modifyrecords.c
@@ -1,12 +1,17 @@
 // Fucntion to modify a customer record
-void modifyRecord(char phoneNumber[])
+void modifyRecord(const char *phoneNumber)
 {
     for (int i = 0; i < customerCount; i++)
     {
         if (strcmp(customer[i].phoneNumber, phoneNumber) == 0)
     {
         printf("\nEnter new usage (in minutes) for %s:", custemers[i].name);
-        scanf("%f", &customers[i].usage);
+        if (scanf("%f", &customers[i].usage)!= 1 ||customers[i].usage<0){
+            printf("Invalid usage amount.\n");
+            clearInputBuffer();
+            return;
+        }
+        clearInputBuffer();
         customers[i].totalBill = customers[i].usage * 0.1;
         printf("\nRecord modified successfully!\n");
         return;
