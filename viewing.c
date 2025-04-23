012287a607a60f94e3c32d0ecd70a6a2d878b1b9
francis19-py <francisgithambo80@gmail.com>
2025-04-23 20:09:08 +0300

    error handling

--- viewing.c
+++ viewing.c
@@ -1,5 +1,8 @@
 //function to view payments
 void viewRecords () {
+    if (customercount == 0){
+        printf("\n No records to display.\n");
+        return 1;
     printf("\n Name\tPhone Number\tUsage(min)\tTotal Bill($)\n");
     for (int i = 0; i < customerCount; i++){
         printf("%s\t%s\t%.2f\t\t%.2f\n", customers[i].name, customers[i].phoneNumber, customers[i].usage, customers[i].TotalBill);
