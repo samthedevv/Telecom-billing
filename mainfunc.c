4d3c88c271c49124169b0180c00211eb14e899b3
francis19-py <francisgithambo80@gmail.com>
2025-04-23 21:00:13 +0300

    Update mainfunc.c

--- mainfunc.c
+++ mainfunc.c
@@ -5,7 +5,13 @@ int main (){
     while(1){
         displayMenu();
         printf("Enter your choice: ");
-        scanf("%d",&choice);
+        if(scanf("%d",&choice) != 1){
+            printf("Invalid input . Please enter a number .\n);
+            clearInputBuffer();
+            continue;
+        }
+        clearInputBuffer();
+        
 
         switch(choice){
         case 1:
@@ -16,22 +22,25 @@ int main (){
                 break;       
         case 3:
                 printf("\nEnter phone number to modify record: ");
-                scanf(" %s", phoneNumber);
+                scanf(" %14s", phoneNumber);
+                clearInputBuffer();
                 modifyRecord(phoneNumber);
                 break;
         case 4:
                 printf("\nEnter phone number to view payment: ");
-                scanf(" %s", phoneNumber);
+                scanf("14%s", phoneNumber);
+                clearInputBuffer();
                 viewpayent(phoneNumber);
                 break;
         case 5:
                 printf("\nEnter phone number to delete record:  ");
-                scanf("%s", phoneNumber);
+                scanf("%14s", phoneNumber);
+                clearInputBuffer();
                 deleteRecord(phoneNumber); 
                 break;
         case 6:
-                return 0;
-            default:
+                printf("exiting the program . bye!");
+        default:
             printf("\n Invalid choice! PLease try again.\n");
 
         }
