f3002d9c30f86c126ee5a149a0de8cf75c55bf89
francis19-py <francisgithambo80@gmail.com>
2025-04-23 21:23:19 +0300

    Update Structure.c

--- Structure.c
+++ Structure.c
@@ -1,19 +1,32 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#define MAX_CUSTOMERS 100
 
 //struct holding customer information
 
 struct customer {
-    char name;
-    char phoneNumber;
+    char name[50];
+    char phoneNumber[15];
     float usage;
     float totalBill;
 };
 //array to store customer data.
-struct customer 
-customers[100] ;
-
+struct customer customers[MAX_CUSTOMERS];
 //global variable for customers
-
 int customerCount = 0;
+//clear  input buffer 
+void clearInputBuffer () {
+    int c;
+    while ((c = getchar ()) != '\n' && c != EOF);
+}
+//validate phonenumber (digits only)
+int isValidPhoneNumber(const char *phone){
+    for(int i = 0; phone[i]; i++){
+        if(!isdigit(phone[i])) return 0;
+    }
+    return 1;
+}
  
