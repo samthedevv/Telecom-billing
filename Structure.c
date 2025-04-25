26d5211f958690eb2f9de1df95f809014f2f8139
francis19-py <francisgithambo80@gmail.com>
2025-04-25 12:40:25 +0300

    viewpayment

--- Structure.c
+++ Structure.c
@@ -84,6 +84,18 @@ void modifyRecord(const char *phoneNumber)
     }
     printf("\nRecord not found!\n");
 }
+
+//function to view payments
+void viewRecords () {
+    if (customerCount == 0){
+        printf("\n No records to display.\n");
+        return 1;
+    printf("\n Name\tPhone Number\tUsage(min)\tTotal Bill($)\n");
+    for (int i = 0; i < customerCount; i++){
+        printf("%s\t%s\t%.2f\t\t%.2f\n", customers[i].name, customers[i].phoneNumber, customers[i].usage, customers[i].TotalBill);
+    }
+}
+}
 	
 
  
