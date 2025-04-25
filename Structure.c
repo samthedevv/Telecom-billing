1553a4c087b5f509ead86e6e560fb3b9cb8a3ca8
francis19-py <francisgithambo80@gmail.com>
2025-04-25 12:36:27 +0300

    modify records

--- Structure.c
+++ Structure.c
@@ -62,6 +62,28 @@ void addRecord()
 		customerCount++; 
 		printf("\nRecord added successfully!\n"); 
 	} 
+
+// Fucntion to modify a customer record
+void modifyRecord(const char *phoneNumber)
+{
+    for (int i = 0; i < customerCount; i++)
+    {
+        if (strcmp(customer[i].phoneNumber, phoneNumber) == 0)
+        {       
+        printf("\nEnter new usage (in minutes) for %s:", customers[i].name);
+        if (scanf("%f", &customers[i].usage)!= 1 ||customers[i].usage<0){
+            printf("Invalid usage amount.\n");
+            clearInputBuffer();
+            return;
+        }
+        clearInputBuffer();
+        customers[i].totalBill = customers[i].usage * 0.1;
+        printf("\nRecord modified successfully!\n");
+        return;
+    }
+    }
+    printf("\nRecord not found!\n");
+}
 	
 
  
